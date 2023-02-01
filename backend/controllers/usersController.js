87700c6383a30a02fcd7ba616a838115b3d8422d
MW <matthewrhyswilliams@live.com>
2023-02-01 20:30:11 +0000

    update route working

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -2,23 +2,35 @@ import { v4 as uuid } from 'uuid';
 
 let users = [];
 
-export const getUsers = (req, res) => {
-  res.send(users);
-}
-
 export const createUser = (req, res) => {
   const user = req.body;
 
   users.push({...user, id: uuid()});
+
   res.send('User added successfully')
+}
+
+export const getUsers = (req, res) => {
+  res.send(users);
 }
 
 export const getUser = (req, res) => {
   const singleUser = users.filter((user) => user.id === req.params.id);
+
   res.send(singleUser);
+}
+
+export const updateUser = (req, res) => {
+  const user = users.find((user) => user.id === req.params.id);
+
+  user.name = req.body.name;
+  user.age = req.body.age;
+
+  res.send('User updated successfully');
 }
 
 export const deleteUser = (req, res) => {
   users = users.filter((user) => user.id !== req.params.id);
+
   res.send('User deleted successfully');
 }
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,11 +1,21 @@
 import express from 'express';
-import { getUsers, createUser, getUser, deleteUser } from '../controllers/usersController.js';
+import { createUser, getUsers, getUser, deleteUser, updateUser } from '../controllers/usersController.js';
 
 const router = express.Router();
 
-router.get('/users', getUsers);
+// Create a new user
 router.post('/user', createUser);
+
+// Retrieve all users
+router.get('/users', getUsers);
+
+// Retrieve a single user by ID
 router.get('/user/:id', getUser);
+
+// Delete a user by ID
 router.delete('/user/:id', deleteUser);
+
+// Update a user by ID
+router.put('/user/:id', updateUser);
 
 export default router;
