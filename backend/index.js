730b96ef9656a437a69337fdcd6f148bfb3ab8ef
MW <matthewrhyswilliams@live.com>
2023-02-06 21:40:00 +0000

    ordering of imports and spacing

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+
+import homeRoute from './routes/HomeRoutes.js';
+import userRoute from './routes/UserRoutes.js';
+
+import { error404, error500 } from './errors/Errors.js';
+
 const app = express();
 const hostname = '127.0.0.1';
 const port = 5000;
 
 app.listen(port, () => console.log(`Server listening on port: http://${hostname}:${port}`));
 
-import cors from 'cors';
 app.use(cors());
 
-import homeRoute from './routes/HomeRoutes.js';
-import userRoute from './routes/UserRoutes.js';
 app.use('/', homeRoute);
 app.use('/users', userRoute);
 
-import { error404, error500 } from './errors/Errors.js';
 app.all('*', error404);
 app.use(error500);
